ae0caaf8ea89cc84a13c7f552058565702716a05
Debayo Tomisin <debayotomisin07@gmail.com>
2021-12-10 10:50:37 -0800

    rendering resturants and categories data, home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useState,} from 'react'
-import { View, Text, TouchableOpacity, Image, StyleSheet, FlatList } from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet, FlatList, SafeAreaView } from 'react-native'
 import { COLORS, FONTS, icons, SIZES } from '../constants'
 
 import { categoryData, initialCurrentLocation, ResturantData } from '../data'
@@ -16,7 +16,7 @@ const Home = ({ navigation }) =>
 
         setResturants(resturantList)
 
-        selectedCategory(category)
+        setSelectedCategory(category)
     }
 
     function getCategoryNameById(id) {
@@ -113,7 +113,7 @@ const Home = ({ navigation }) =>
             )
         }
         return(
-            <View>
+            <View style={{ padding: SIZES.padding * 2 }}>
                 <Text style={{ ...FONTS.h1 }}>Main</Text>
                 <Text style={{ ...FONTS.h1 }}>Categories</Text>
 
@@ -129,9 +129,83 @@ const Home = ({ navigation }) =>
         )
     }
 
-    return(
-        <View style={{}}>
+    function renderResturantList() {
+        const renderItem = ({item }) => (
+            <TouchableOpacity
+                style={{ marginBottom: SIZES.padding * 2 }}
+                onPress={ () => navigation.navigate("Resturant", { item, currentLocation })}
+            >
+                <View style={{ marginBottom: SIZES.padding }}>
+                    <Image source={item.photo} resizeMode='cover' style={{ width: '100%', height: 200, borderRadius: SIZES.radius }} />
+                    <View
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            height: 50,
+                            width: SIZES.width * 0.3,
+                            backgroundColor: COLORS.white,
+                            borderTopRightRadius: SIZES.radius,
+                            borderBottomLeftRadius: SIZES.radius,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            ...styles.shadow
+                        }} 
+                    >
+                        <Text style={{ ...FONTS.h4 }}>{item.duration} </Text>
+                    </View>
+                </View>
 
+                <Text style={{...FONTS.body2}}>{item.name}</Text>
+
+                <View style={{ marginTop: SIZES.padding, flexDirection: 'row' }}>
+                    <Image source={icons.star} style={{ height:20, width:20, tintColor:COLORS.primary, marginRight: 10 }} />
+                    <Text style={{...FONTS.body3}}>{item.rating}</Text>
+                </View>
+
+                <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                    {
+                        item.categories.map((categoryId, idx) => {
+                            return(
+                                <View key={idx} style={{ flexDirection: 'row' }}>
+                                    <Text style={{ ...FONTS.body3 }}>{getCategoryNameById(categoryId)}</Text>
+                                    <Text style={{ ...FONTS.h3, color: COLORS.darkgray }}> . </Text>
+                                </View>
+                            )
+                        })
+                    }
+
+                    {
+                        [1, 2, 3].map(priceRating => (
+                            <Text 
+                                key={priceRating} 
+                                style={{ ...FONTS.body3, color: (priceRating <= item.priceRating) ? COLORS.black : COLORS.darkgray}}>
+                                    $
+                                </Text>
+                        ))
+                    }
+                </View>
+
+            </TouchableOpacity>
+        )
+
+        return(
+            <FlatList
+                data={resturants}
+                keyExtractor={item => `${item.id}`}
+                renderItem={renderItem}
+                contentContainerStyle={{
+                    paddingHorizontal: SIZES.padding * 2,
+                    paddingBottom: 30
+                }} 
+            />
+        )
+    }
+
+    return(
+        <View style={styles.container}>
+            {renderHeader()}
+            {renderMainCategories()}
+            {renderResturantList()}
         </View>
     )
 }
