242614e42403b757748683c3742dc8c6fe628dc7
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-02 21:37:13 -0800

    header section of resturants page

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -75,10 +75,62 @@ const Resturant = ({ route, navigation }) =>
 
         return total.toFixed(2)
     }
+    
+    function renderHeader() {
+        return(
+            <View style= {{flexDirection: 'row'}}>
+                <TouchableOpacity 
+                    style={{
+                        width: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Image source={icons.back} resizeMode='contain' style={{ width: 30, height: 30 }} />
+                </TouchableOpacity>
+
+                <View style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                    <View 
+                        style={{
+                            height: 50,
+                            alignItems:'center',
+                            justifyContent: 'center',
+                            paddingHorizontal: SIZES.padding * 2,
+                            borderRadius: SIZES.radius,
+                            backgroundColor: COLORS.lightGray3
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h3}}>{resturant?.name}</Text>
+                    </View>
+                </View>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight:SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.list}
+                        resizeMode='contain'
+                        style={{
+                            width: 30,
+                            height: 30,
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text>How dare you</Text>
+            {renderHeader()}
         </SafeAreaView>
     )
 }
