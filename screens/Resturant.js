e1dd48722e27f407f54ccc2c037db5bdc5b68852
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-02 22:49:25 -0800

    render dish info section of resturants page

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -128,9 +128,108 @@ const Resturant = ({ route, navigation }) =>
         )
     }
 
+    function renderFoodInfo() {
+        return(
+            <Animated.ScrollView
+                horizontal
+                pagingEnabled
+                scrollEventThrottle={16}
+                snapToAlignment="center"
+                showsHorizontalScrollIndicator={false}
+                onScroll={ Animated.event([
+                    { nativeEvent : { contentOffset: { x: scrollX }}}
+                ], { useNativeDriver: false })}
+            >
+                {
+                    resturant?.menu.map((item, index) => (
+                        <View style={{ alignItems: 'center' }} key={`menu-${index}`}>
+                            <View style={{ height: SIZES.height * 0.35}}>
+                                <Image
+                                    source={item.photo}
+                                    resizeMode='cover'
+                                    style={{
+                                        width:SIZES.width,
+                                        height: "100%"
+                                    }}
+                                />
+                                <View
+                                    style={{
+                                        position: 'absolute',
+                                        bottom: -20,
+                                        width: SIZES.width,
+                                        height: 50,
+                                        justifyContent: 'center',
+                                        flexDirection: 'row',
+                                    }} 
+                                >
+                                    <TouchableOpacity
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderTopLeftRadius: 25,
+                                            borderBottomLeftRadius: 25
+                                        }}
+                                        onPress={() => editOrder("-", item.menuId, item.price)}
+                                    >
+                                        <Text style={{ ...FONTS.body1 }}>-</Text>
+                                    </TouchableOpacity>
+
+                                    <View
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center'
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.h2 }}>{getOrderQantity(item.menuId)}</Text>
+                                    </View> 
+
+                                    <TouchableOpacity
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderTopRightRadius: 25,
+                                            borderBottomRightRadius: 25
+                                        }}
+                                        onPress={() => editOrder("+", item.menuId, item.price)}
+                                    >
+                                        <Text style={{ ...FONTS.body1}}>+</Text>
+                                    </TouchableOpacity>    
+                                </View>
+                            </View>
+
+                            <View
+                                style={{
+                                    width: SIZES.width,
+                                    alignItems: 'center',
+                                    marginTop: 15,
+                                    paddingHorizontal: SIZES.padding * 2
+                                }}
+                            >
+                                <Text style={{ marginVertical: 10, textAlign: 'center', ...FONTS.h2 }}>{item.name} - {item.price.toFixed(2)}</Text>
+                                <Text style={{...FONTS.body3}}>{item.description}</Text>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                                <Image source={icons.fire} style={{ height: 20, width: 20, marginRight: 10 }} />
+                                <Text style={{ ...FONTS.h2, color: COLORS.darkgray }}>{item.calories.toFixed(2)} cal</Text>
+                            </View>
+                        </View>
+                    ))
+                }
+            </Animated.ScrollView>
+        )
+    }
+
     return(
         <SafeAreaView style={styles.container}>
             {renderHeader()}
+            {renderFoodInfo()}
         </SafeAreaView>
     )
 }
