6a916dc9a09d07ac8b43cf66523494f72e4845f2
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-02 23:15:15 -0800

    page navigation animations

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -226,10 +226,68 @@ const Resturant = ({ route, navigation }) =>
         )
     }
 
+    function renderDots() {
+        const dotPosition = Animated.divide(scrollX, SIZES.width)
+        return (
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: SIZES.padding
+            }}>
+                {
+                    resturant?.menu.map((item, index) => {
+
+                        const opacity = dotPosition.interpolate({
+                            inputRange: [index - 1, index, index + 1],
+                            outputRange: [0.3, 1, 0.3],
+                            extrapolate: 'clamp'
+                        })
+
+                        const dotSize = dotPosition.interpolate({
+                            inputRange: [index - 1, index, index + 1],
+                            outputRange: [SIZES.base * 0.8, 10, SIZES.bases * 0.8 ],
+                            extrapolate: 'clamp'
+                        })
+
+                        const dotColor = dotPosition.interpolate({
+                            inputRange: [index - 1, index, index + 1],
+                            outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+                            extrapolate: 'clamp'
+                        })
+
+                        return (
+                            <Animated.View
+                                key={`dot-${index}`}
+                                opacity={opacity}
+                                style={{
+                                    borderRadius: SIZES.radius,
+                                    marginHorizontal: 6,
+                                    width: dotSize,
+                                    height: dotSize,
+                                    backgroundColor: dotColor
+                                }}
+                            />
+                        )
+                    })
+                }
+            </View>
+        )
+    }
+
+    function renderOrder() {
+        return(
+            <View>
+                { renderDots()}
+            </View>
+        )
+    }
+
     return(
         <SafeAreaView style={styles.container}>
             {renderHeader()}
             {renderFoodInfo()}
+            {renderOrder()}
         </SafeAreaView>
     )
 }
