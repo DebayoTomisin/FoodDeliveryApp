c83e90b84daf7b15569b8ee80f89f761d1d484c8
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-20 09:00:55 -0800

    resurnt page

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -298,7 +298,72 @@ const Resturant = ({ route, navigation }) =>
                         <Text style={{ ...FONTS.h3 }}>{sumOrder()}</Text>
                     </View>
 
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            paddingVertical: SIZES.padding * 2,
+                            paddingHorizontal: SIZES.padding * 3
+                        }}
+                    >
+                        <View style={{ flexDirection: 'row' }}>
+                            <Image
+                                source={icons.pin}
+                                resizeMode='contain'
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                    tintColor: COLORS.darkgray
+                                }}
+                            />
+                            <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>Location</Text>
+                        </View>
+
+                        <View style={{ flexDirection: "row"}}>
+                            <Image  
+                                source={icons.master_card} 
+                                resizeMode='contain' 
+                                style={{ height: 20, width: 20, tintColor:COLORS.darkgray}}
+                            />
+                            <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>8888</Text>
+                        </View>
+                    </View>
+
+                    <View style= {{
+                        padding: SIZES.padding * 2,
+                        alignItems: 'center',
+                        justifyContent: "center"
+                    }}>
+                        <TouchableOpacity
+                            style={{
+                                width: SIZES.width * 0.9,
+                                padding: SIZES.padding,
+                                backgroundColor: COLORS.primary,
+                                alignItems: 'center',
+                                borderRadius: SIZES.radius
+                            }}
+                            onPress={() => navigation.navigate("OrderDelivery", {
+                                resturant: resturant,
+                                currentLocation: currentLocation
+                            })}
+                        >
+                            <Text style={{ color: COLORS.white, ...FONTS.h2 }}>Order</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
+                {
+                    isIphoneX() && 
+                    <View
+                        style={{
+                            position: 'absolute',
+                            bottom: -34,
+                            left: 0,
+                            right: 0,
+                            height: 34,
+                            backgroundColor: COLORS.white
+                        }}
+                    ></View>
+                }
             </View>
         )
     }
